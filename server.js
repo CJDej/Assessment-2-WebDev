42c97f7313b9424c6699c782fe5ae98a0ea5af32
CJDej <dejoldechristianjames@gmail.com>
2026-01-21 16:36:21 +0000

    Update server port and add gitignore

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 1. Serve Static Content (HTML, CSS, JS) from the 'public' folder
 app.use(express.static('public'));
